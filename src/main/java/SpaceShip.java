ce26ce160edd62672390a8d9ff42aded69209bc6
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-30 15:46:00 +0200

    Task completed 3

--- src/main/java/SpaceShip.java
+++ src/main/java/SpaceShip.java
@@ -6,6 +6,9 @@ public class SpaceShip {
     }
 
     public void setName(String name) {
+        if (name.strip().length() > 100 || name.isBlank()) {
+            return;
+        }
         this.name = name;
     }
 
@@ -15,5 +18,14 @@ public class SpaceShip {
         System.out.println(ship.getName()); //Should be null
         ship.setName("Walker");
         System.out.println(ship.getName()); //Should be Walker
+
+        ship.setName("Walker");
+        System.out.println(ship.getName()); //Should be Walker
+
+        ship.setName("");
+        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+
+        ship.setName("Voyager ".repeat(100));
+        System.out.println(ship.getName()); //Should be Walker, too long value ignored
     }
 }
