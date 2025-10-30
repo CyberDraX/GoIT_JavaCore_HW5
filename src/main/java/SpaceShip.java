954df953cef00dd7659740bcdbc752d892300377
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-30 15:28:23 +0200

    Task completed 2

--- src/main/java/SpaceShip.java
+++ src/main/java/SpaceShip.java
@@ -1,3 +1,19 @@
 public class SpaceShip {
-    public String name = "Voyager";
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        SpaceShip ship = new SpaceShip();
+
+        System.out.println(ship.getName()); //Should be null
+        ship.setName("Walker");
+        System.out.println(ship.getName()); //Should be Walker
+    }
 }
