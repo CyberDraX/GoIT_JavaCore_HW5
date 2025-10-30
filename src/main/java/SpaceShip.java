af005c128a2a88cb707400ed5cf15a234c4f479b
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-10-30 19:47:29 +0200

    Task completed 5

--- src/main/java/SpaceShip.java
+++ src/main/java/SpaceShip.java
@@ -25,26 +25,36 @@ public class SpaceShip {
         this.serialNumber = serialNumber;
     }
 
+    public void printInfo() {
+        System.out.println("Name is " + name + ", serial number is " + serialNumber);
+    }
+
     public static void main(String[] args) {
         SpaceShip ship = new SpaceShip();
 
-        System.out.println(ship.getName()); //Should be null
-        ship.setName("Walker");
-        System.out.println(ship.getName()); //Should be Walker
+//        System.out.println(ship.getName()); //Should be null
+//        ship.setName("Walker");
+//        System.out.println(ship.getName()); //Should be Walker
+//
+//        ship.setName("Walker");
+//        System.out.println(ship.getName()); //Should be Walker
+//
+//        ship.setName("");
+//        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+//
+//        ship.setName("Voyager ".repeat(100));
+//        System.out.println(ship.getName()); //Should be Walker, too long value ignored
+//
+//        ship.setSerialNumber("SN506788");
+//        System.out.println(ship.getSerialNumber()); //Should be SN506788
+//
+//        ship.setSerialNumber("EE123456");
+//        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
 
-        ship.setName("Walker");
-        System.out.println(ship.getName()); //Should be Walker
-
-        ship.setName("");
-        System.out.println(ship.getName()); //Should be Walker, empty value ignored
-
-        ship.setName("Voyager ".repeat(100));
-        System.out.println(ship.getName()); //Should be Walker, too long value ignored
-
+        ship.setName("Voyager");
         ship.setSerialNumber("SN506788");
-        System.out.println(ship.getSerialNumber()); //Should be SN506788
 
-        ship.setSerialNumber("EE123456");
-        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
+        //Name is Voyager, serial number is SN504030
+        ship.printInfo();
     }
 }
