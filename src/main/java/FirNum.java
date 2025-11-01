b048999d2e8b3d9de49c331690f65ebfe12024c1
Vadim Korolov <vadim.v.korolov@gmail.com>
2025-11-01 16:46:57 +0200

    Task completed 28

--- src/main/java/FirNum.java
+++ src/main/java/FirNum.java
@@ -20,11 +20,26 @@ class FirTest {
         System.out.println(new FirNumSum().calc(3));
         System.out.println(new FirNumSum().calc(84)); //3570
         System.out.println(new FirNumSum().calc(0)); // 0
+
+        //Should be 120
+        System.out.println(firTest.test(firNum, 5));
+        System.out.println(new FirNumFactorial().calc(5));
     }
 }
 class FirNumSum extends FirNum {
     @Override
     public int calc(int n) {
         return n * (n + 1) / 2;
+    }
+}
+class FirNumFactorial extends FirNum {
+
+    @Override
+    public int calc(int n) {
+        int result = 1;
+        for (int i = 1; i <= n ; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
